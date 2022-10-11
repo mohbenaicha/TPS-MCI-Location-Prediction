d2cf0fc62a33321927e4e1b2963c44a868b4b4d5
mohben <mohamed.benaicha@hotmail.com>
2022-10-11 11:40:21 -0400

    flush

--- model_package/tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from mci_model.config.base import config
-from mci_model.utilities.data_manager import load_dataset
-
-
-@pytest.fixture()
-def test_input_data():
-    return load_dataset(file_name=config.app_config.test_data_file)
